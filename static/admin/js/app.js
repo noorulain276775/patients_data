693434f63a9098a522822ab7372e91b2cffe2de6
Noor ul ain Ibrahim <noorulainibrahim75@gmail.com>
2022-08-21 15:39:18 +0400

    View all records done

--- static/admin/js/app.js
+++ static/admin/js/app.js
@@ -1,5 +1,12 @@
 const spinnerbox = document.getElementById('spinner-box')
+const alert_box = document.getElementById('alertbox')
+console.log(alert_box)
+console.log(spinnerbox)
 
-function spinner(){
+function spinner() {
     spinnerbox.classList.remove('not-visible')
 }
+
+function buttonclose(){ 
+    alert_box.classList.add('not-visible'); 
+ };
